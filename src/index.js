4c20403ac3001a0d09fa668f6e302cfb99b8d116
DmitryForsilov <tpadtpad@yandex.ru>
2020-02-19 17:45:36 +0300

    add badge

--- src/index.js
+++ src/index.js
@@ -1,5 +1,7 @@
 import readlineSync from 'readline-sync';
 
+console.log('Welcome to the Brain Games!');
+
 export default () => {
   const answer = readlineSync.question('May I have your name?');
   console.log(`Hello, ${answer}!`);
