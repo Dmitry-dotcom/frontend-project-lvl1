3f23b6e69cf54dca42938b6e727e45248e40a9b7
DmitryForsilov <tpadtpad@yandex.ru>
2020-02-25 11:29:55 +0300

    refactored gcd

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -3,21 +3,26 @@ import engine from '../engine.js';
 
 const taskDescription = 'Find the greatest common divisor of given numbers.';
 
+const getGcd = (a, b) => {
+  const divisor = a < b ? a : b;
+
+  for (let i = divisor; i > 0; i -= 1) {
+    if (a % i === 0 && b % i === 0) {
+      return i;
+    }
+  }
+
+  return false;
+};
+
 const getGameData = () => {
   const a = getRandomNum();
   const b = getRandomNum();
 
   const question = `${a} ${b}`;
-  const divisor = a < b ? a : b;
+  const correctAnswer = getGcd(a, b);
 
-  for (let i = divisor; i > 0; i -= 1) {
-    if (a % i === 0 && b % i === 0) {
-      const correctAnswer = i;
-      return [question, correctAnswer];
-    }
-  }
-
-  return false;
+  return [question, correctAnswer];
 };
 
 export default () => engine(taskDescription, getGameData);
